5c0d1ad9a89bb94779cb3bde1fb6b24e21fcf7d6
Ashish <ashishnagpal2498@gmail.com>
2020-05-31 22:37:46 +0530

    :up: profile and files component

--- src/components/Files.js
+++ src/components/Files.js
@@ -1,10 +1,23 @@
 import React, {Component} from 'react';
-
+import '../css/files.css'
 class Files extends Component {
     render() {
+        // Search - and sort -
         return (
-            <div>
-                
+            <div className="files-container">
+                <h1 className="files-heading">Files</h1>
+                <ul className="card-div">
+                    {[1,2,3,4,5,6].map((item,index) =>
+                        <li className="card" key={index}>
+                            <div className="card-image-outer">
+                                <img src="/assets/videoFile.png" className="card-image" alt={"card-img"} />
+                            </div>
+                            <div className="card-detail">
+                                <h3>Name of file</h3>
+                            </div>
+                        </li>
+                    )}
+                </ul>
             </div>
         );
     }
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,29 @@
 import React, {Component} from 'react';
-
+import '../css/profile.css'
 class Profile extends Component {
     render() {
         return (
-            <div>
+            <div className="profile-container">
+                <div className="profile-avatar">
+                    <img src="/assets/dummy_image.png" alt="profile avatar" />
+                </div>
+                <div className="profile-details-container">
+                    <div className="profile-details">
+                        <div className="fx-b100 dFlexRow">
+                            <h3 className="fx-b50">Name: </h3>
+                            <p className="fx-b50"> ABC</p>
+                        </div>
+                        <div className="fx-b100 dFlexRow mt-5">
+                            <h3 className="fx-b50">Email: </h3>
+                            <p className="fx-b50"> xyz@gmail.com</p>
+                        </div>
+                        <div className="fx-b100 dFlexRow mt-5">
+                            <h3 className="fx-b50">Number of files stored: </h3>
+                            <p className="fx-b50">2</p>
+                        </div>
 
+                    </div>
+                </div>
             </div>
         );
     }
