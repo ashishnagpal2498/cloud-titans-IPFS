a5b015936330a9d7f5cb7b52f021261cf27193f9
Ashish <ashishnagpal2498@gmail.com>
2020-05-31 22:36:26 +0530

    :up: react router correction

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import {BrowserRouter as Router,
 Switch,
 Route} from 'react-router-dom'
+import './css/common.css'
 import Login from "./components/Login";
+import Main from "./components/Main";
 
 function App() {
   return (
     <Router>
         <Switch>
             <Route path="/login" component={Login}/>
+            <Route path={"/user"} component={Main} />
         </Switch>
     </Router>
   );
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,36 +1,38 @@
 import React, {Component} from 'react';
-import '../css/user.css'
-import {BrowserRouter as Router, Switch,Route,Link} from 'react-router-dom'
+import '../css/main.css'
+import {Switch,Route,Link} from 'react-router-dom'
 import Profile from "./Profile";
+import Files from "./Files";
+import Upload from "./Upload";
 class Main extends Component {
     state = {
 
     };
     render() {
+        console.log(this.props);
         return (
-            <div className="container">
+            <div className="main-container">
                 <div className="side-nav">
                     <div className="user-avatar-wrapper">
                         <div className="user-avatar">
-                        <imh src={""} alt="profile" />
+                        <img src={""} alt="profile" />
                         </div>
                     </div>
                     <ul className="navigation">
                         <li>
-                            <Link to={"/profile"}> Profile</Link>
+                            <Link to={"/user/profile"}> Profile</Link>
                         </li>
-                        <li><Link to={"/upload"}>Upload</Link></li>
-                        <li><Link to={"/files"}>View Files</Link></li>
+                        <li><Link to={"/user/upload"}>Upload</Link></li>
+                        <li><Link to={"/user"}>View Files</Link></li>
                     </ul>
                 </div>
                 <div className="main-content">
-                    <Router>
                         <Switch>
-                            <Route exact path="/profile" component={Profile}/>
-                            <Route exact path="/upload" component={""} />
-                            <Route path={"/"} component={""} />
+                            <Route path={`${this.props.match.path}/profile`} component={Profile} />
+                            <Route path={`${this.props.match.path}/upload`} component={Upload} />
+                            <Route path={"/"} component={Files} />
                         </Switch>
-                    </Router>
+
                 </div>
             </div>
         );
