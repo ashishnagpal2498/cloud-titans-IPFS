241a16555cbe59a284d2acb812cf3076750e6758
Ashish <ashishnagpal2498@gmail.com>
2020-06-05 23:40:56 +0530

    :star: responsive

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ import Files from "./Files";
 import Upload from "./Upload";
 class Main extends Component {
     state = {
-
+        toggleMenu : false
     };
     componentWillMount() {
 
@@ -14,29 +14,38 @@ class Main extends Component {
     componentDidMount() {
         console.log(this.props);
     }
-
+    toggleMenu = () => {
+            this.setState((prevState) => {
+                return{
+                    toggleMenu : !prevState.toggleMenu
+                }
+            })
+    }
     render() {
         return (
             <div className="main-container">
-                <div className="side-nav">
+                <div className={this.state.toggleMenu ? "side-nav toggle-menu": "side-nav"}>
                     <div className="user-avatar-wrapper">
                         <div className="user-avatar">
                         <img src={"/assets/dummy_image.png"} alt="profile" />
                         </div>
                     </div>
                     <ul className="navigation">
-                        <li>
+                        <li onClick={this.state.toggleMenu && this.toggleMenu}>
                             <Link to={`${this.props.match.url}/profile/`}> Profile</Link>
                         </li>
-                        <li><Link to={`${this.props.match.url}/upload/`}>Upload</Link></li>
-                        <li><Link to={`/user/${this.props.match.params.id}`}>View Files</Link></li>
+                        <li onClick={this.state.toggleMenu && this.toggleMenu}><Link to={`${this.props.match.url}/upload/`}>Upload</Link></li>
+                        <li onClick={this.state.toggleMenu && this.toggleMenu} ><Link to={`/user/${this.props.match.params.id}`}>View Files</Link></li>
                     </ul>
+                </div>
+                <div className="nav-menu" onClick={this.toggleMenu}>
+                    <i className="fa fa-bars" />
                 </div>
                 <div className="main-content">
                         <Switch>
                             <Route exact path={`${this.props.match.path}/profile`} component={Profile} />
                             <Route exact path={`${this.props.match.path}/upload`} component={Upload} />
-                            <Route path={"/"} component={Files} />
+                            <Route path={"/"} id={this.props.match.params.id} component={Files} />
                         </Switch>
 
                 </div>
